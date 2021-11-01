566e0b891ace31f0b853ed98496bfad03298b2dc
Luke <74873871+LucasCampardo@users.noreply.github.com>
2021-10-31 21:32:41 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what is your name?");
+            string name = (Console.ReadLine());
+            Console.WriteLine("Nice to meet you, " + name);
+        }
+    }
+}
